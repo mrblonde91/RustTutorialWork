87d03be6d393aaebc9fd765ffd5d43971a1847f7
mmurphyglewis <mmurphy@glasslewis.com>
2019-05-19 21:49:23 +0100

    Further rust work

---
+++ enumtest/src/main.rs
@@ -0,0 +1,22 @@
+// fn main() {
+//     let four = IpAddrKind::V4;
+//     let six = IpAddrKind::V6;
+
+//     let home = IpAddrKind::V4(127, 0, 0, 1);
+//     let loopback = IpAddrKind::V6(String::from("::1"));
+// }
+
+// fn route(ip_type:IpAddrKind){
+
+// }
+
+
+// //Possible to store within an enum as demonstrated above
+// enum IpAddrKind{
+//     V4(u8,u8,u8,u8),
+//     V6(String),
+// }
+
+fn main(){
+    
+}
--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,44 @@
 fn main() {
-    println!("Hello, world!");
+    let rectangle = Rectangle{width:30, height:70};
+    println!("The area of the rectangle is {} square pixels....", areatuplerefactoringstruct(&rectangle));
+    let square = Rectangle::square(20);
+    println!("Debug print of {:#?}", rectangle);
+
+    println!("Square is {:?}", square);
+    //Struct specific function
+    println!("Result of built in functionality is {}", rectangle.area());
+}
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
+
+// fn areatuplerefactoringtuple(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+ fn areatuplerefactoringstruct(rectangle: &Rectangle) -> u32{
+    rectangle.width * rectangle.height
+ }
+
+// POC of debug
+#[derive(Debug)]
+struct Rectangle{
+    width: u32,
+    height: u32,
+}
+
+//Rectangle specific functions, --implementation block--
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool{
+        self.width > other.width && self.height > other.height
+    }
+    
+    fn square(size:u32) -> Rectangle{
+        Rectangle{height:size, width:size}
+    }
 }
