f643ff33820b34d15c0b445ac5f0b5e701c4065d
Michael Murphy <5879863+mrblonde91@users.noreply.github.com>
2019-09-02 22:33:13 +0100

    Added file reading to minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+//12.01 minigrep
+//use std::env::args_os if not valid unicode
+// collect arguments from the iterator
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    let query = &args[1];
+    let filename = &args[2];
+    let contents = fs::read_to_string(filename)
+    .expect("The file does not exist");
+    println!("Contents of file:\n {}", contents);
 }
