5e85c5ea9b89fd2e835a2d5efcdba808610ee36a
Michael Murphy <mmurphy@glasslewis.com>
2019-09-03 12:04:46 +0100

    Extracted logic of parsing

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,10 +5,20 @@ use std::fs;
 // collect arguments from the iterator
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-    let query = &args[1];
-    let filename = &args[2];
-    let contents = fs::read_to_string(filename)
+    let config = parse_config(&args);
+    let contents = fs::read_to_string(config.filename)
     .expect("The file does not exist");
     println!("Contents of file:\n {}", contents);
+}
+
+struct Config {
+    filename: String,
+    query: String,
+}
+
+//clone gives a copy to own
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config{filename, query}
 }
