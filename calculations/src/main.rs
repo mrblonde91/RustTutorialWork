4a6c9941325d769e17201ade87e430f635df15ed
Michael Murphy <5879863+mrblonde91@users.noreply.github.com>
2019-09-04 17:35:38 +0100

    Thread sleeping

--- calculations/src/main.rs
+++ calculations/src/main.rs
@@ -2,14 +2,12 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    simulated_expensive_calculation(2);
-}
-
-fn simulated_expensive_calculation(intensity: u32) -> u32{
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
 
-    generate_workout(simulated_user_specified_value, simulated_random_number);
+    generate_workout(simulated_user_specified_value, simulated_random_number);}
+
+fn simulated_expensive_calculation(intensity: u32) -> u32{
     println!("Calculating slowly");
     thread::sleep(Duration::from_secs(2));
     intensity
@@ -19,10 +17,10 @@ fn generate_workout(intensity: u32, random_number: u32) {
     if intensity < 25 {
         println!("Today do {} pushups", simulated_expensive_calculation(intensity));
         println!("Next do {} situps", simulated_expensive_calculation(intensity));
-    }else{
+    } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
-        }else{
+        } else {
             println!("Today, run for {} minutes", simulated_expensive_calculation(intensity));
         }
     }
