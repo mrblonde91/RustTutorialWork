9f2c729b9b2ef58c5d49747e6b3a979cd1aa2d43
Michael Murphy <5879863+mrblonde91@users.noreply.github.com>
2019-09-08 21:13:32 +0100

    started adding hashmap

--- calculations/src/main.rs
+++ calculations/src/main.rs
@@ -1,6 +1,8 @@
 use std::thread;
 use std::time::Duration;
+use std::collections::HashMap;
 
+//13.01 Closures
 fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
@@ -9,19 +11,58 @@ fn main() {
 
 fn generate_workout(intensity: u32, random_number: u32) {
     //type will get inferred on first
-    let expensive_closure = |num| {
+    let mut expensive_closure = Cacher::new(|num| {
         println!("Calculating slowly");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
     if intensity < 25 {
-        println!("Today do {} pushups", expensive_closure(intensity));
-        println!("Next do {} situps",  expensive_closure(intensity));
+        println!("Today do {} pushups", expensive_closure.value(intensity));
+        println!("Next do {} situps",  expensive_closure.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!("Today, run for {} minutes",  expensive_closure(intensity));
+            println!("Today, run for {} minutes",  expensive_closure.value(intensity));
         }
     }
+}
+
+//Implement caching
+struct Cacher<T> where T: Fn(u32) -> u32{
+    calculation: T,
+    value: HashMap<u32>,
+    }
+
+impl<T> Cacher<T> 
+    where T: Fn(u32) -> u32{
+        fn new(calculation: T) -> Cacher<T>{
+            Cacher{
+                calculation,
+                value: None,
+                }
+        }
+
+        fn value(&mut self, arg: u32) -> u32{
+            // execute calculation if value not populated
+            match self.value{
+                Some(v) => v,
+                None => {
+                    let v = (self.calculation)(arg);
+                    self.value = Some(v);
+                    v
+                },
+            }
+        }
+    }
+
+
+//Try implementation with hashmap to fix this.
+#[test]
+fn call_with_different_values(){
+    let mut c = Cacher::new(|a| a);
+    let v1  = c.value(1);
+    let v2 = c.value(2);
+
+    assert_eq!(v2, 2);
 }
