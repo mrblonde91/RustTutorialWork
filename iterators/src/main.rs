0121f5b6cfc8382d76aff37fafe74844ddd5719d
Michael Murphy <5879863+mrblonde91@users.noreply.github.com>
2019-09-09 17:21:13 +0100

    Updated minigrep to use iterators

---
+++ iterators/src/main.rs
@@ -0,0 +1,43 @@
+//13.02 - Iterators are basically way to implement for loops
+fn main() {
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+}
+
+#[test]
+fn iterators_creating_new_iterators(){
+    let v1: Vec<i32> = vec![1, 2, 3];
+    //this creates a new iterator, has to be collected to be consumed.
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2,3,4]);
+}
+
+//Can call next method in iterator manually
+#[test]
+fn iterator_demonstration(){
+    let v1 = vec![1, 2, 3];
+
+    // has to be defined as mutable to borrow
+    let mut v1_iter = v1.iter();
+
+    //Next is a consuming adapter, each time it's called, it's iterator is removed.
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(),None);
+}
+
+#[test]
+fn iterator_sum(){
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+    //v1_iter can't be used after summing as it has taken ownership.
+    let total: i32 = v1_iter.sum();
+
+    assert_eq!(total, 6);
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,8 +6,7 @@ use minigrep::Config;
 //use std::env::args_os if not valid unicode
 // collect arguments from the iterator
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         //eprintln prints to standard errors
         eprintln!("Problems parsing arguments: {}", err);
         process::exit(1);
